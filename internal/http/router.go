22c097135693e9fa690b4ac1744f0aaf86d0cb1a
joseCamposdeveloper <josecamposdeveloper@gmail.com>
2025-10-07 16:07:30 +0200

    add print

--- internal/http/router.go
+++ internal/http/router.go
@@ -11,7 +11,7 @@ import (
 
 func NewRouter(cfg config.Config) http.Handler {
 	r := chi.NewRouter()
-
+	println("CORSAllowedOrigins: ", cfg.CORSAllowedOrigins)
 	// Middleware CORS
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   cfg.CORSAllowedOrigins,
